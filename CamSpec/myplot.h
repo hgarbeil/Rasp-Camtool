24276d86670b5371705bdcea029028a80d64e947
Harold <garbeil@hawaii.edu>
2016-12-10 06:17:50 +0000

    Working QT display

---
+++ CamSpec/myplot.h
@@ -0,0 +1,18 @@
+#ifndef MYPLOT_H
+#define MYPLOT_H
+
+#include <QCustomPlot>
+
+class MyPlot : public QCustomPlot
+{
+    Q_OBJECT
+public:
+    explicit MyPlot(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // MYPLOT_H
